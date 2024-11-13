124252fb92dc0733723f94436e22927e3889db6a
GuilhermeDanzer <guidanzer@hotmail.com>
2024-11-12 21:42:03 -0300

    clear eslint

--- app/components/firstAct.tsx
+++ app/components/firstAct.tsx
@@ -1,5 +1,4 @@
 'use client'
-import Image from 'next/image'
 import styles from './page.module.css'
 import { useRef, useState } from 'react'
 import bg from '@/public/forest.jpg'
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import { useState } from 'react'
 import FirstAct from './components/firstAct'
 
 export default function Home() {
-  const [open, setOpen] = useState(false)
-  const [blackout, setBlackout] = useState(false)
   const [currentAct, setCurrentAct] = useState(0)
   const acts = [<FirstAct setCurrentAct={setCurrentAct} />]
   return <>{acts[currentAct]}</>
